5e124195a9763d361bfdc939549fac90c08809f9
Paritchayanon <140487319+Paritchaynon@users.noreply.github.com>
2024-10-02 00:39:58 +0700

    fix form

--- assets/js/main.js
+++ assets/js/main.js
@@ -241,4 +241,60 @@
   }
   window.addEventListener("load", navmenuScrollspy);
   document.addEventListener("scroll", navmenuScrollspy);
+
+  /**
+   * PHP Email Form Validation - This is probably using a third party library
+   */
+  
+  let forms = document.querySelectorAll('.php-email-form');
+  forms.forEach( function(e) {
+    e.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      let thisForm = this;
+
+      let action = thisForm.getAttribute('action');
+      if( ! action ) {
+        displayError(thisForm, 'The form action property is not set!');
+        return;
+      }
+
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData( thisForm );
+
+      fetch(action, {
+        method: 'POST',
+        body: formData,
+        headers: {'Accept': 'application/json'}
+      })
+      .then(response => {
+        if( response.ok ) {
+          return response.json();
+        } else {
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+        }
+      })
+      .then(data => {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        if (data.ok) {
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset(); 
+        } else {
+          throw new Error(data.error || 'Form submission failed and no error message returned from: ' + action); 
+        }
+      })
+      .catch((error) => {
+        displayError(thisForm, error);
+      });
+    });
+  });
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
 })();
